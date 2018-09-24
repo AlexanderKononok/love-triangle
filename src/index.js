e0505cc1b3ca9a65389526851032d0f18de9db0d
AlexanderKononok <alex.kononok@gmail.com>
2018-09-24 15:50:12 +0300

    Implementation the task

--- src/index.js
+++ src/index.js
@@ -4,4 +4,16 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let count = 0;
+  
+  for (let i = 0; i < preferences.length; i++) {
+    let firstSpichonee = preferences[i];
+    let secondSpichonee = preferences[firstSpichonee - 1];
+    let thirdSpichonee = preferences[secondSpichonee - 1];
+    if (thirdSpichonee === i + 1) {
+      count++;
+    }
+  }
+
+  return Math.floor(count / 3);
 };
